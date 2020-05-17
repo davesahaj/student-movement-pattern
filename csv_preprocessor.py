a64e3d6642f3dd074d56ef45e8d114a8d79cf298
dave <sahajdave@gmail.com>
2020-05-17 08:45:05 +0530

    preprocess compiled csv files

---
+++ csv_preprocessor.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+
+def converter(group):
+    for i in group:
+        print("creating sheet-"+str(i))
+        fname = "data/csv/file-Sheet"+str(i)+".csv"
+        df = pd.read_csv(fname, usecols=["Date", "HH:MM:SS", "Wifi Id"])
+        df.rename(columns={df.columns[1]: "Time"}, inplace=True)
+
+        dates = []
+        time = []
+
+        for d in df.Date:
+            d = d[0:10]
+            dates.append(d)
+
+        for t in df.Time:
+            t = t[0:5]
+            time.append(t)
+
+        df['Date'] = dates
+        df['Time'] = time
+        fname = "data/trimmed/file-Sheet"+str(i)+".csv"
+
+        df.to_csv(fname, columns=["Date", "Time",
+                                  "Wifi Id"], index=None, header=None)
+
+group = [6, 7, 8, 9, 10]
+converter(group)
