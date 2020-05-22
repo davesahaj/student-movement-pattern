f34b925f3314f95ef20342dd0d4d02b3c5148383
dave <sahajdave@gmail.com>
2020-05-22 11:44:47 +0530

    working date wise frequency

--- frequency.py
+++ frequency.py
@@ -1,29 +1,58 @@
 import pandas as pd
+import dateutil
 
 
-def counter(group):
+def frequency(group):
+    interval = [1, 2, 3, 4, 6, 8, 12]
+    start_hour = '02:00'
+    end_hour = '04:00'
+    s = []
+
     for i in group:
-        print("creating sheet-counted-"+str(i))
-        fname = "data/counted/file-Sheet"+str(i)+".csv"
-        df = pd.read_csv(fname)
-        fname = "data/frequency/file-Sheet"+str(i)+".csv"
+        s.append("data/counted/file-Sheet"+str(i)+".csv")
+        # df['Date'] = df['Date'].apply(dateutil.parser.parse,dayfirst=True)
 
-        count = df.groupby(['Wifi Id']).size()
-       # count = 
+    df = pd.read_csv(s[0])
+    itrfile = iter(s)
+    next(itrfile)
 
-        print(count)
-        count.to_csv(fname)
+    frames = []
 
-        df = pd.read_csv(fname)
-        df.rename(columns={df.columns[1]: "Frequency"}, inplace=True)
-        df.to_csv(fname)
+    for x in s:
+        print(x)
+        df1 = pd.read_csv(x)
+        df = df.append(df1)
+
+    df['Date'] = df['Date'].apply(dateutil.parser.parse, dayfirst=True)
+    df = df.sort_values(by='Date')
+
+    dates = []
+    for d in df['Date'].dt.date:
+        if d not in dates:
+            dates.append(d)
+
+    for x in dates:
+        frame = (df.loc[df['Date'].dt.date == x])
+        frame.set_index('Date', inplace=True)
+        frame = (frame.loc[frame['Wifi Id'] == '"Canteen"'])
+       
+
+       # frame = (frame.between_time(start_hour, end_hour))
+       # frame = (frame.groupby('Wifi Id').count())
+        frame.to_csv("data/frequency/"+str(x)+".csv")
+
+    # df.to_csv("out.csv")
+
+
+group1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+          26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
 
 
 
-group1 = [6, 7, 8, 9, 10]
 group2 = [3, 4, 5, 14, 16, 19, 20, 21]
 group3 = [2, 13, 11, 15]
 
-counter(group1)
-counter(group2)
-counter(group3)
+frequency(group1)
+# frequency(group1)
+# frequency(group2)
+# frequency(group3)
