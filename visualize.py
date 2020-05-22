b8c581504a9484419216d6424e69e28a909f6980
dave <sahajdave@gmail.com>
2020-05-22 11:45:21 +0530

    redone graphs

--- visualize.py
+++ visualize.py
@@ -16,16 +16,35 @@ def visual(group):
     itrfile = iter(s)
     next(itrfile)
 
+    frames = []
+
     for x in s:
+        print(x)
         df1 = pd.read_csv(x)
-        df.merge(df1, left_on='Wifi Id', right_on='Wifi Id')
-
-
-    
+        df = df.append(df1)
 
     fname = "data/visual/file-Sheet"+str(i)+".csv"
-    df.to_csv(fname, index=None)
- 
+
+   # $count = df.groupby(['Wifi Id']).count()
+    df.to_csv(fname,index=None)
+
+   # df = pd.read_csv(fname)
+  #  df.rename(columns={df.columns[1]: "Frequency"}, inplace=True)
+   # df.drop(df[df.Frequency < 3].index, inplace=True)
+   # df=df.sort_values(by=['Frequency'])
+   # df.to_csv(fname)
+
+    #df = pd.read_csv(fname)
+
+   # x = df['Wifi Id']
+   # y = df["Frequency"]
+   # plt.bar(x, y, label="Example one", align='center', width=0.3)
+
+
+#    plt.title('Most visited places')
+
+    #plt.show()
+
 
 group1 = [6, 7, 8, 9, 10]
 group2 = [3, 4, 5, 14, 16, 19, 20, 21]
