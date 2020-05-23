6e2778528676b36ff12b4d21075011d145d3ad7e
dave <sahajdave@gmail.com>
2020-05-23 17:50:34 +0530

    added driver program

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import shutil
+
+if not os.path.exists("data"):
+    os.makedirs("data/csv")
+shutil.copyfile("file.xlsx", "data/file.xlsx")
+
+os.system("python xlsToCsvConverter.py data/file.xlsx")
+os.system("python csv_preprocessor.py")
+os.system("python unique_wifi.py")
+os.system("python frequency.py")
+os.remove("data/file.xlsx")
